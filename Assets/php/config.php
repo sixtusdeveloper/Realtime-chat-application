cf5459b73d53f477023b509b80e257133d4b24bf
sixtusdev <sixtusushahemba@gmail.com>
2023-02-17 17:49:31 +0100

    commit on all files and folders

--- Assets/php/config.php
+++ Assets/php/config.php
@@ -1,8 +1,8 @@
 <?php
 $hostname = 'localhost';
-$username = 'sixtus';
+// $username = 'sixtus';
 $password = 's1i9x9t9us/log';
-$dbname = 'sixtus';
+// $dbname = 'sixtus';
 
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
 if (!$conn) {
